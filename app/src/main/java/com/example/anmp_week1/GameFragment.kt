dfc60be1e0fce9278f380d50f51fef8b4d722828
unknown <nathanaelsteven06@gmail.com>
2025-02-19 17:32:59 +0700

    init

---
+++ app/src/main/java/com/example/anmp_week1/GameFragment.kt
@@ -0,0 +1,30 @@
+package com.example.anmp_week1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.anmp_week1.databinding.FragmentGameBinding
+
+class GameFragment : Fragment() {
+    private lateinit var binding: FragmentGameBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container:
+    ViewGroup?,savedInstanceState: Bundle?): View? {
+        binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val playerName =
+            GameFragmentArgs.fromBundle(requireArguments()).playerName
+        binding.txtTurn.text = "$playerName's Turn"
+        binding.btnBack.setOnClickListener {
+            val action = GameFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/anmp_week1/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.anmp_week1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.anmp_week1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.hostFragment)
+                    as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
+
+
+}
